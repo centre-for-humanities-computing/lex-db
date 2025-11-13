3210e671b11158de02d4e53cb6c1b5bada42cb60
Simon Enni <simon.enni@gmail.com>
2025-11-13 10:39:32 +0100

    Added automatic deployment for lex-db and cleaned up the staging deployment scripts (#41)

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Reloading systemctl daemon"
 sudo systemctl daemon-reload
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Enabling lex-db-staging service"
 sudo systemctl enable lex-db-staging.service
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Restarting lex-db-staging service"
 sudo systemctl restart lex-db-staging.service
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Checking lex-db-staging service status"
 sudo systemctl status lex-db-staging.service 
 exit 0
